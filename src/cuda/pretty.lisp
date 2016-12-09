a75bd60497ea518bf23f8a649772dd258fce1388
lispbub <lispbub@users.noreply.github.com>
2016-12-09 17:26:36 +0100

    cuda pretty printer adapted to initializer list fix

--- src/cuda/pretty.lisp
+++ src/cuda/pretty.lisp
@@ -1,44 +1,5 @@
 (in-package :cm-cuda)
 
-;;; copy from c pretty.lisp
-;;; restores original behaviour
-;;; Declaration item
-;;; Handle declaration assignment.
-(with-pp
-  (with-proxynodes (value)
-
-    (defprettymethod :before declaration-item
-      (make-proxy value value))
-
-    (defprettymethod :after declaration-item
-      (del-proxy value))
-
-    (defproxyprint :before value
-      (if (slot-value item 'proxy-subnode)
-	  (format stream " = ")))))
-
-;;; copy from ast-pretty.lisp
-;;; restores original behavious
-;;; C-list / vectors
-(with-pp
-  (with-proxynodes (list-item)
-
-    (defprettymethod :before clist
-      (make-proxy items list-item)
-      (push-sign 'skip-first-clist)
-      (format stream "{ "))
-
-    (defprettymethod :after clist
-      (del-proxy items)
-      (when (eql (top-sign) 'skip-first-clist)
-	(pop-sign))
-      (format stream " }"))
-
-    (defproxyprint :before list-item
-      (if (eql (top-sign) 'skip-first-clist)
-	  (pop-sign)
-	  (format stream ", ")))))
-
 ;;; ========================
 ;;;   cuda pretty printing
 ;;; =========================
