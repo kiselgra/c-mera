96cc0188da72fe4dfbb07133d9cfc725c08e05d5
kai <kai@istar>
2017-10-25 21:26:00 +0200

    extended testcase according to #91

--- tests/c.decl.00.lisp
+++ tests/c.decl.00.lisp
@@ -3,7 +3,10 @@
 (function foo2 () -> void (return))
 
 (decl ((void (fpointer nam0 ((int))) = foo)
-       (void (fpointer nam1 ()) = foo2)))
+       (void (fpointer nam1 ()) = foo2)
+       (void (fpointer nam2 ((unsigned int ui))))
+       (void (fpointer nam2 (int int))) ; just a suggestion
+       ))
 
 (function main () -> int (printf "0\\n") (return 0))
 
