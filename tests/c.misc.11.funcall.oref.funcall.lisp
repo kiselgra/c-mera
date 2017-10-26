9a1ed0263c7dc94f73bedfb41e6e36a97f3ebb46
kai <kai@istar>
2017-10-26 13:20:16 +0200

    Test case for #93.

---
+++ tests/c.misc.11.funcall.oref.funcall.lisp
@@ -0,0 +1,23 @@
+(include <stdio.h>)
+
+;; simple function returning int
+(function foo-fn () -> int (return 0))
+ 
+;; typedef for a function returning int, taking no arguments
+(typedef int (fpointer foo-fn-ptr ()))
+
+;; function that takes one (useless) argument and returns a function such as foo-fn.
+(function bar-fn1 ((int x)) -> foo-fn-ptr (return foo-fn))
+
+;; function that takes two (useless) arguments and returns a function such as foo-fn.
+(function bar-fn2 ((int x) (int y)) -> foo-fn-ptr (return foo-fn))
+
+(function main ((int argc) (char **argv)) -> int
+(decl ((int x = 0)
+       (int y = 0)
+       (int a = (funcall (funcall bar-fn1 x)))
+       (int b = (funcall (funcall bar-fn2 x y)))))  ;; Bug #93: x y are pasted to become xy.
+  (printf "0\\n")
+  (return 0))
+
+;;## 0
